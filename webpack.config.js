5b6416e53f247e099da4b2dc90024183cdf9d2cd
Matroskin94 <anton1994anton@mail.ru>
2018-03-17 12:05:56 +0300

    Adding packages for redux and routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/App.jsx',
+    entry: path.join(__dirname, 'src', 'App.jsx'),
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
