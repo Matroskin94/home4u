ae3318480f5178916020a543d219a39d81f62768
Matroskin94 <anton1994anton@mail.ru>
2018-03-19 10:57:04 +0300

    Adding of material-ui package, adding of loaders for css

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import initStore from '../store/appStore';
 import HomePage from './HomePage.jsx';
 import RegistrationPage from './RegistrationPage.jsx';
 import LogInPage from './LogInPage.jsx';
+import '../styles/styles.css';
 
 const store = initStore();
 
+
 const AppRouter = props => (
     <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route
-                    exact
-                    path='/'
-                    component={HomePage}
-                />
-                <Route path='/registration' component={RegistrationPage} />
-                <Route path='/login' component={LogInPage} />
+        <MuiThemeProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        exact
+                        path='/'
+                        component={HomePage}
+                    />
+                    <Route path='/registration' component={RegistrationPage} />
+                    <Route path='/login' component={LogInPage} />
 
-            </Switch>
-        </BrowserRouter>
+                </Switch>
+            </BrowserRouter>
+        </MuiThemeProvider>
     </Provider>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,17 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
             }
         ]
     },
