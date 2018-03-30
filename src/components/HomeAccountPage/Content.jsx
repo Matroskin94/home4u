03673c4d5525ea6c8ca539d279c6c2160a183c61
Matroskin94 <anton1994anton@mail.ru>
2018-03-30 16:12:50 +0300

    Added MyHomePageWrapper for elements of Home page

---
+++ src/components/HomeAccountPage/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from 'material-ui/styles';
+import { Typography } from 'material-ui';
+import PropTypes from 'prop-types';
+import stylesJS from './stylesJSS/ContentStylesJS';
+
+const Content = ({ classes }) => (
+    <div className={classes.container}>
+        <Typography>Hello smart house</Typography>
+    </div>
+);
+
+Content.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(stylesJS)(Content);
--- src/pages/userPages/HomeAccountPage.jsx
+++ src/pages/userPages/HomeAccountPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import MyHomePageWrapper from './MyHomePageWrapper.jsx';
+import Content from '../../components/HomeAccountPage/Content.jsx';
 
 const HomeAccountPage = props => (
-    <h1>Account information</h1>
+    <MyHomePageWrapper>
+        <Content />
+    </MyHomePageWrapper>
 );
 
 export default HomeAccountPage;
